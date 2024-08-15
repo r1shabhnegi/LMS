4a5524e40fe5e69b546de43fe680834218cb0591
irishabhnegi <rishabhnegi175@gmail.com>
2024-08-15 13:26:25 +0530

    Reids, Db and cloudinary connections

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+require("dotenv").config();
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -10,3 +11,21 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // cors (cross origin resource sharing)
+app.use(cors({ origin: process.env.ORIGIN }));
+
+// testing api
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+// unknown route
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
