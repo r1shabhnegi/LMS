fee06f1d114e8bf5ac3ae5d603edccb4adb0a641
irishabhnegi <rishabhnegi175@gmail.com>
2024-08-31 21:53:54 +0530

    layout route

---
+++ server/controllers/layout.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import LayoutModel from "../models/layout.model";
+import cloudinary from "cloudinary";
+// create layout
+export const createLayout = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.body;
+      if (type === "Banner") {
+        const { image, title, subTitle } = req.body;
+        const myCloud = await cloudinary.v2.uploader.upload(image, {
+          folder: "layout",
+        });
+        const banner = {
+          image: {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+          },
+        };
+        await LayoutModel.create(banner);
+      }
+
+      if (type === "FAQ") {
+        const { faq } = req.body;
+        await LayoutModel.create(faq);
+      }
+      if (type === "FAQ") {
+        const { categories } = req.body;
+        await LayoutModel.create(categories);
+      }
+      res.status(200).json({
+        success: true,
+        message: "Layout created successfully",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
