7251fc554244ede147900c19cc47eb7b90c4b8da
irishabhnegi <rishabhnegi175@gmail.com>
2024-09-01 00:51:20 +0530

    update layout

--- server/controllers/layout.controller.ts
+++ server/controllers/layout.controller.ts
@@ -9,6 +9,12 @@ export const createLayout = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { type } = req.body;
+      const isTypeExist = await LayoutModel.findOne({ type });
+
+      if (isTypeExist) {
+        return next(new ErrorHandler(`${type} already exist`, 400));
+      }
+
       if (type === "Banner") {
         const { image, title, subTitle } = req.body;
         const myCloud = await cloudinary.v2.uploader.upload(image, {
@@ -19,21 +25,113 @@ export const createLayout = CatchAsyncError(
             public_id: myCloud.public_id,
             url: myCloud.secure_url,
           },
+          title,
+          subTitle,
         };
         await LayoutModel.create(banner);
       }
 
       if (type === "FAQ") {
         const { faq } = req.body;
-        await LayoutModel.create(faq);
+        const faqItems = await Promise.all(
+          faq.map(async (item: any) => {
+            return {
+              question: item.question,
+              answer: item.answer,
+            };
+          })
+        );
+        await LayoutModel.create({ type: "FAQ", faq: faqItems });
       }
-      if (type === "FAQ") {
+
+      if (type === "Categories") {
         const { categories } = req.body;
-        await LayoutModel.create(categories);
+        const categoriesItems = await Promise.all(
+          categories.map(async (item: any) => {
+            return {
+              title: item.title,
+            };
+          })
+        );
+        await LayoutModel.create({
+          type: "Categories",
+          categories: categoriesItems,
+        });
       }
       res.status(200).json({
         success: true,
         message: "Layout created successfully",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// edit layout
+
+export const editLayout = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.body;
+
+      if (type === "Banner") {
+        const bannerData: any = await LayoutModel.findOne({ type: "Banner" });
+        const { image, title, subTitle } = req.body;
+        if (bannerData) {
+          await cloudinary.v2.uploader.destroy(bannerData.image.public_id);
+        }
+        const myCloud = await cloudinary.v2.uploader.upload(image, {
+          folder: "layout",
+        });
+        const banner = {
+          image: {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+          },
+          title,
+          subTitle,
+        };
+        await LayoutModel.findByIdAndUpdate(bannerData.id, { banner });
+      }
+
+      if (type === "FAQ") {
+        const { faq } = req.body;
+        const faqItem = await LayoutModel.findOne({ type: "FAQ" });
+        const faqItems = await Promise.all(
+          faq.map(async (item: any) => {
+            return {
+              question: item.question,
+              answer: item.answer,
+            };
+          })
+        );
+        await LayoutModel.findByIdAndUpdate(faqItem?._id, {
+          type: "FAQ",
+          faq: faqItems,
+        });
+      }
+
+      if (type === "Categories") {
+        const { categories } = req.body;
+        const categoriesItem = await LayoutModel.findOne({
+          type: "Categories",
+        });
+        const categoriesItems = await Promise.all(
+          categories.map(async (item: any) => {
+            return {
+              title: item.title,
+            };
+          })
+        );
+        await LayoutModel.findByIdAndUpdate(categoriesItem?._id, {
+          type: "Categories",
+          categories: categoriesItems,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: "Layout updated successfully",
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { createLayout } from "../controllers/layout.controller";
+import { createLayout, editLayout } from "../controllers/layout.controller";
 
-const layoutRouter = express.Router()
+const layoutRouter = express.Router();
 
-layoutRouter.post('/create-layout',isAuthenticated,authorizeRoles("admin"), createLayout)
+layoutRouter.post(
+  "/create-layout",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  createLayout
+);
 
-export default layoutRouter
+layoutRouter.put(
+  "/edit-layout",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editLayout
+);
+
+export default layoutRouter;
