f59f995f24edfd8c35aed5cdf11c4dc4f662dfd7
irishabhnegi <rishabhnegi175@gmail.com>
2024-09-01 13:19:54 +0530

    client setup

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React, { FC, useState } from "react";
+
+interface Props {}
+
+const Page: FC<Props> = () => {
+  return <div></div>;
+};
+
+export default Page;
--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -209,7 +209,9 @@ export const updateAccessToken = CatchAsyncError(
       const session = await redis.get(decoded.id as string);
 
       if (!session) {
-        return next(new ErrorHandler(message, 400));
+        return next(
+          new ErrorHandler("Please login for access this resources", 400)
+        );
       }
 
       const user = JSON.parse(session);
@@ -230,7 +232,7 @@ export const updateAccessToken = CatchAsyncError(
       req.user = user;
       res.cookie("access_token", accessToken, accessTokenOptions);
       res.cookie("refresh_token", refreshToken, refreshTokenOptions);
-
+      await redis.set(user._id, JSON.stringify(user), "EX", 604800); // 7days
       res.status(200).json({
         status: "success",
         accessToken,
