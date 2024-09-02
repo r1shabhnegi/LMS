56701ec766a5ff9ca96386b3a0ac1f2e2697e12d
irishabhnegi <rishabhnegi175@gmail.com>
2024-09-02 23:42:36 +0530

    client- header

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,28 @@
 "use client";
-import React, { FC, useState } from "react";
-
+import React, { FC, use, useState } from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
 interface Props {}
 
 const Page: FC<Props> = () => {
-  return <div></div>;
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  return (
+    <div>
+      <Heading
+        title='ELearing'
+        description='ELearning is a platform for students to learn and get help from teachers'
+        keywords='programming, MERN, Redux, Machine Learning'
+      />
+      <Header
+        open={open}
+        activeItem={activeItem}
+        setOpen={setOpen}
+      />
+      <Hero />
+    </div>
+  );
 };
 
 export default Page;
