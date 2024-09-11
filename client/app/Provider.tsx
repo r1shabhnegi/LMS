db6526f6212fb051237f284df20b6cd1f409ca5a
irishabhnegi <rishabhnegi175@gmail.com>
2024-09-11 15:23:27 +0530

    redux toolkit and RTKQuery setup and registration user

---
+++ client/app/Provider.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactNode } from "react";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
+
+interface ProviderProps {
+  children: ReactNode;
+}
+export const Providers = ({ children }: ProviderProps) => {
+  return <Provider store={store}>{children}</Provider>;
+};
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { Poppins } from "next/font/google";
 import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./utils/theme-provider";
 import { Toaster } from "react-hot-toast";
+import { Providers } from "./Provider";
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -23,16 +26,18 @@ export default function RootLayout({
     <html lang='en'>
       <body
         className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem>
-          {children}
-          <Toaster
-            position='top-center'
-            reverseOrder={false}
-          />
-        </ThemeProvider>
+        <Providers>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem>
+            {children}
+            <Toaster
+              position='top-center'
+              reverseOrder={false}
+            />
+          </ThemeProvider>
+        </Providers>
       </body>
     </html>
   );
