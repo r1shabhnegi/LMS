3581ea6e2acc675709a4d080447cca8850749b69
irishabhnegi <rishabhnegi175@gmail.com>
2024-11-06 13:45:46 +0530

    cors update4

--- server/app.ts
+++ server/app.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // cors (cross origin resource sharing)
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://learnmaxnow.vercel.app"],
+    origin: "https://learnmaxnow.vercel.app",
     credentials: true,
   })
 );
