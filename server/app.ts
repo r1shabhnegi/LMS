eb669ad5ae8c1769472c593e4f1d1f48cbcb2f3f
irishabhnegi <rishabhnegi175@gmail.com>
2024-11-06 14:08:46 +0530

    cors update6

--- server/app.ts
+++ server/app.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // cors (cross origin resource sharing)
 app.use(
   cors({
-    origin: "https://lms-two-omega.vercel.app",
+    origin: "*",
     credentials: true,
   })
 );
